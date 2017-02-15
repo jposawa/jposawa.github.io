c2dab823201f4bba8a46f93acd8c60b291c7b14a
jposawa <jp.osawa@hotmail.com>
2017-02-15 18:42:04 -0300

    amostra interface interativa v1

---
+++ redrocket/comportamento.js
@@ -0,0 +1,15 @@
+var logado = false;
+var pagAnterior = "inicio.html";
+
+$(document).ready(function()
+{
+	//alert(pagAnterior);
+	$("#corpoPagina").load("inicio.html");
+
+});
+
+function carregaPagina(url)
+{
+	//alert(url);
+	$("#corpoPagina").css("inicio.html");
+}
