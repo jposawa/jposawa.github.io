a3f7abe95ea0596ded4bedea39740e2ff83bc64b
jposawa <jposawa@gmail.com>
2019-12-18 11:03:39 -0300

    Upload da build

---
+++ react/tictactoemodificado/jogar/precache-manifest.1baef6df78e19b5537ef844fee3c0af9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16851e6926d3e5b26d9c8de87debb5b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97ddec1d5657eaf8d6a7",
+    "url": "/static/css/main.6a534e88.chunk.css"
+  },
+  {
+    "revision": "ee45084456e82309d982",
+    "url": "/static/js/2.c1809de3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c1809de3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "97ddec1d5657eaf8d6a7",
+    "url": "/static/js/main.124292a4.chunk.js"
+  },
+  {
+    "revision": "177d52efa2c7db582a05",
+    "url": "/static/js/runtime-main.5b50bb1e.js"
+  }
+]);
